8594d2324d47dd2c87769fdfe5245148f1836cb3
omar-dev0 <11410120211067@stud.cu.edu.eg>
2024-08-19 00:30:57 +0300

    add poppins font add flutter_screen_util package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return ScreenUtilInit(
+      designSize:   const Size(430,932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child:  ScreenUtilInit(
+        builder: (context , child)=>MaterialApp(
+          home: child
+        ),
+        child: const Scaffold(
+          body: Center(
+            child: Text('Hello World!'),
+          ),
         ),
       ),
     );
