5e5f63b9e298149dd42834813c5d87b0d6c02ac0
Myproject <mohamedAlaa-CS>
2024-08-20 23:35:13 +0300

    create total price and checkout button widget

--- lib/features/presentation/cart/screens/cart_screen.dart
+++ lib/features/presentation/cart/screens/cart_screen.dart
@@ -3,7 +3,9 @@ import 'package:ecommerce_app/core/resources/color_manager.dart';
 import 'package:ecommerce_app/core/resources/styles_manager.dart';
 import 'package:ecommerce_app/core/resources/values_manager.dart';
 import 'package:ecommerce_app/features/presentation/cart/widgets/cart_item_widget.dart';
+import 'package:ecommerce_app/features/presentation/cart/widgets/total_price_and_checkout_botton.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -56,10 +58,16 @@ class CartScreen extends StatelessWidget {
                   colorName: 'Black',
                 ),
                 separatorBuilder: (context, index) =>
-                    const SizedBox(height: AppSize.s14),
+                    SizedBox(height: AppSize.s14.h),
                 itemCount: 2,
               ),
             ),
+            // the total price and checkout button========
+            TotalPriceAndCheckoutBotton(
+              totalPrice: 1200,
+              checkoutButtonOnTap: () {},
+            ),
+            SizedBox(height: 10.h),
           ],
         ),
       ),
---
+++ lib/features/presentation/cart/widgets/total_price_and_checkout_botton.dart
@@ -0,0 +1,53 @@
+import 'package:ecommerce_app/core/resources/color_manager.dart';
+import 'package:ecommerce_app/core/resources/styles_manager.dart';
+import 'package:ecommerce_app/core/resources/values_manager.dart';
+import 'package:ecommerce_app/core/widget/custom_elevated_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TotalPriceAndCheckoutBotton extends StatelessWidget {
+  const TotalPriceAndCheckoutBotton(
+      {super.key, required this.totalPrice, required this.checkoutButtonOnTap});
+  final int totalPrice;
+  final void Function() checkoutButtonOnTap;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          children: [
+            // Total price ==================================
+            Text(
+              'Total price',
+              style: getMediumStyle(
+                color: ColorManager.textColor.withOpacity(0.6),
+                fontSize: AppSize.s18.sp,
+              ),
+            ),
+            SizedBox(height: 9.h),
+            Text(
+              'EGP $totalPrice',
+              style: getMediumStyle(
+                color: ColorManager.textColor,
+                fontSize: AppSize.s18.sp,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(width: 18.h),
+
+        // Checkout button ================================
+        Expanded(
+          child: CustomElevatedButton(
+            data: 'Checkout',
+            onTap: checkoutButtonOnTap,
+            suffixIcon: Icon(
+              Icons.arrow_forward,
+              color: ColorManager.white,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
